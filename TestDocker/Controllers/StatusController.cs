1e0fcaa37b4dc587f92e1cbc04eabd93108ff046
Oleh Karasiov <karasiovoleg@gmail.com>
2021-06-07 00:46:07 +0300

    done

---
+++ TestDocker/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using TestDocker.Services;
+using Repository.Interfaces;
+namespace TestDocker.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private IBuildVersionRepository service;
+
+        public StatusController(IBuildVersionRepository service)
+        {
+            this.service = service;
+        }
+        [HttpGet()]
+        public async Task<IActionResult> GetBuildVersion()
+        {
+            var hello = "Hello World";
+            var res = await service.GetBuildVersion();
+            var result = Newtonsoft.Json.JsonConvert.SerializeObject(new { message = hello, DatabaseVersion = res });
+            return Ok(result);
+        }
+ 
+    }
+}
---
+++ TestDocker/Services/BuildVersionService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Repository.DbModels;
+using Repository.Interfaces;
+
+namespace TestDocker.Services
+{
+    public class BuildVersionService : IBuildVersionRepository
+    {
+
+        private DbSet<BuildVersionRecord> _dbSet;
+        public BuildVersionService(AppDbContext context)
+        {
+            _dbSet = context.BuildVersion;
+            
+        }
+        public async Task<BuildVersionRecord> GetBuildVersion()
+        {
+            var result = await _dbSet.FirstOrDefaultAsync();
+            return result;
+        }
+    }
+}
